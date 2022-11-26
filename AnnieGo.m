fbbfbf5d2c75ed3e4b713335bb93c69db5c9d9d5
Cooooooooooookie <106728054+Cooooooooooookie@users.noreply.github.com>
2022-11-26 22:36:26 +0800

    Add files via upload

---
+++ AnnieGo.m
@@ -0,0 +1,53 @@
+
+function [go_row, go_column] = AnnieGo(board)
+    
+    weight = 4;
+    o_weight = 1;
+    x_weight = 1;
+    
+    score = zeros(10);
+    
+    for i = 1:6
+        for j = 1:10
+            %\\\ Horizontal_line ///%
+            o_num = length(find(board(j,i:i+4) == 1));
+            x_num = length(find(board(j,i:i+4) == 2));
+            if o_num == 0 || x_num == 0
+                score(j,i:i+4) = score(j,i:i+4) + (o_num * o_weight + x_num * x_weight + 1) ^ weight;
+            end
+            
+            %\\\ Vertical_line ///%
+            o_num = length(find(board(i:i+4,j) == 1));
+            x_num = length(find(board(i:i+4,j) == 2));
+            if o_num == 0 || x_num == 0
+                score(i:i+4,j) = score(i:i+4,j) + (o_num * o_weight + x_num * x_weight + 1) ^ weight;
+            end
+        end
+        
+        for j = 1:6
+            %\\\ Left_slash ///%
+            o_num = length(find(diag(board(i:i+4,j:j+4)) == 1));
+            x_num = length(find(diag(board(i:i+4,j:j+4)) == 2));
+            if  o_num == 0 || x_num == 0
+                for k = 1:5
+                    score(i+k-1,j+k-1) = score(i+k-1,j+k-1) + (o_num * o_weight + x_num * x_weight + 1) ^ weight;
+                end
+            end
+            
+            %\\\ Right_slash ///%
+            o_num = length(find(diag(fliplr(board(j:j+4,i:i+4))) == 1));
+            x_num = length(find(diag(fliplr(board(j:j+4,i:i+4))) == 2));
+            if  o_num == 0 || x_num == 0
+                for k = 1:5
+                    score(j+k-1,i-k+5) = score(j+k-1,i-k+5) + (o_num * o_weight + x_num * x_weight + 1) ^ weight;
+                end
+            end
+        end
+    end
+    
+    %\\\ Already have pieces ///%
+    score(find(board)) = 0;
+    
+    max_point = find(score == max(max(score))) - 1;
+    go_row = mod(max_point(1), 10) + 1;
+    go_column = fix(max_point(1) / 10) + 1;

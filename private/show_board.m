718ed654d4530a1d1e967a8049f2c6ab9e504d8c
Cooooooooooookie <bingkanglin@gmail.com>
2022-12-08 01:58:50 +0800

    # Code Refactoring

---
+++ private/show_board.m
@@ -0,0 +1,32 @@
+function x = show_board(board)
+
+    clc;
+    
+    fprintf('  ');
+    [side_length, ~] = size(board);
+    for i = 97:96 + side_length
+        fprintf('   %s',i);
+    end
+    fprintf('\n   ');
+    for i = 1:side_length
+        fprintf('----');
+    end
+    fprintf('-\n')
+    for i = 1:side_length
+        fprintf('%02s |',num2str(side_length+1-i));
+        for j=1:side_length
+            if board(i,j) == 0
+                fprintf('   |');
+            elseif board(i,j) == 1
+                fprintf(' O |');
+            elseif board(i,j) == 2
+                fprintf(' X |');
+            end
+        end
+        fprintf('\n   ');        
+        for i = 1:side_length
+            fprintf('----')   
+        end
+        fprintf('-\n')
+    end
+end

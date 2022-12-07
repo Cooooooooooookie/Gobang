e72c364bf062e57d3ebd9366e46668c8353941b7
LarryWu510 <larrywu510@gmail.com>
2022-12-07 21:28:12 +0800

    + First hand or second hand

--- PVE.m
+++ PVE.m
@@ -5,7 +5,15 @@ O_win = 0; X_win = 0;
 x_return = ['a', '1', '1'];
 row = 1;
 column = 1;
-for round = 1:m^2
+fprintf('First or Second?\n');
+fs = input('Please enter F or S\n','s');
+if fs == 'F'
+    r = 1;
+else
+    r = 2;
+end
+    
+for round = r:m^2
     
     show_board(board);
     [O_win, X_win] = win_or_even(board, row, column, m);
